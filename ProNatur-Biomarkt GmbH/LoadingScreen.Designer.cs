9580a044e870dbaced06743c0f67610151842c40
FALK\mft <feit.markus@online.de>
2023-01-12 09:12:21 +0100

    add project

---
+++ ProNatur-Biomarkt GmbH/LoadingScreen.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace ProNatur_Biomarkt_GmbH
+{
+    partial class LoadingScreen
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pbarLoading = new System.Windows.Forms.ProgressBar();
+            this.lblLoading = new System.Windows.Forms.Label();
+            this.lblProgPercent = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.loadingbarTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbarLoading
+            // 
+            this.pbarLoading.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.pbarLoading.Location = new System.Drawing.Point(30, 287);
+            this.pbarLoading.Name = "pbarLoading";
+            this.pbarLoading.Size = new System.Drawing.Size(619, 23);
+            this.pbarLoading.TabIndex = 0;
+            // 
+            // lblLoading
+            // 
+            this.lblLoading.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lblLoading.AutoSize = true;
+            this.lblLoading.ForeColor = System.Drawing.Color.White;
+            this.lblLoading.Location = new System.Drawing.Point(291, 271);
+            this.lblLoading.Name = "lblLoading";
+            this.lblLoading.Size = new System.Drawing.Size(45, 13);
+            this.lblLoading.TabIndex = 1;
+            this.lblLoading.Text = "Loading";
+            // 
+            // lblProgPercent
+            // 
+            this.lblProgPercent.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.lblProgPercent.AutoSize = true;
+            this.lblProgPercent.ForeColor = System.Drawing.Color.White;
+            this.lblProgPercent.Location = new System.Drawing.Point(342, 271);
+            this.lblProgPercent.Name = "lblProgPercent";
+            this.lblProgPercent.Size = new System.Drawing.Size(21, 13);
+            this.lblProgPercent.TabIndex = 2;
+            this.lblProgPercent.Text = "0%";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProNatur_Biomarkt_GmbH.Properties.Resources.GroceryImg;
+            this.pictureBox1.InitialImage = global::ProNatur_Biomarkt_GmbH.Properties.Resources.GroceryImg;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(642, 256);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // loadingbarTimer
+            // 
+            this.loadingbarTimer.Tick += new System.EventHandler(this.loadingbarTimer_Tick);
+            // 
+            // LoadingScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.ClientSize = new System.Drawing.Size(684, 322);
+            this.ControlBox = false;
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblProgPercent);
+            this.Controls.Add(this.lblLoading);
+            this.Controls.Add(this.pbarLoading);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoadingScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ProNatur-Biomarkt GmbH";
+            this.Load += new System.EventHandler(this.LoadingScreen_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pbarLoading;
+        private System.Windows.Forms.Label lblLoading;
+        private System.Windows.Forms.Label lblProgPercent;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer loadingbarTimer;
+    }
+}
+
---
+++ ProNatur-Biomarkt GmbH/LoadingScreen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProNatur_Biomarkt_GmbH
+{
+    public partial class LoadingScreen : Form
+    {
+        static Random r = new Random();
+        public LoadingScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void loadingbarTimer_Tick(object sender, EventArgs e)
+        {
+            if (pbarLoading.Value < 95) pbarLoading.Value += r.Next(5);
+            if (pbarLoading.Value < 100) pbarLoading.Value += 1;
+            lblProgPercent.Text = pbarLoading.Value.ToString() + "%";
+
+            if(pbarLoading.Value > pbarLoading.Maximum)
+            {
+                pbarLoading.Value = pbarLoading.Maximum;
+                loadingbarTimer.Stop();
+            }
+        }
+
+        private void LoadingScreen_Load(object sender, EventArgs e)
+        {
+            loadingbarTimer.Start();
+        }
+    }
+}
